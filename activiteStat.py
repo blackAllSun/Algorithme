f127a145fcee17ff909a98e87df961df48f54b44
Le Hegaret Viviane <attilavlh10@gmail.com>
2017-08-19 21:58:28 +0200

    Update activiteStat.py

--- activiteStat.py
+++ activiteStat.py
@@ -3,7 +3,6 @@
 #
 #  activiteStat.py
 #  
-#  Copyright 2017 BlackAllSun <attilavlh10@gmail.com>
 #  
 from pylab import *
 tabTempsParcours=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,35,38,40,45,50,55,60,90];
