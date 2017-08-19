fc300136c804e329076ceee1fee82891f10b0a77
Le Hegaret Viviane <attilavlh10@gmail.com>
2017-08-19 21:57:47 +0200

    Update activiteStatFicheRenseignement.cpp

--- activiteStatFicheRenseignement.cpp
+++ activiteStatFicheRenseignement.cpp
@@ -1,12 +1,6 @@
 /*
  * activitStatFichesRenseignementEleves.cpp
  * 
- * Copyright 2017 BlackAllSun <attilavlh10@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
  */
 
 #include <map>
